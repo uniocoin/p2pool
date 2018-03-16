5511f26ac1e0a05aa0bc50f5216f6c7ff742fbf4
Wan Hao Jun <wanhaojun118@hotmail.com>
2018-03-16 13:23:16 +0800

    change parameter for uniocoin

--- p2pool/dash/networks/dash.py
+++ p2pool/dash/networks/dash.py
@@ -8,10 +8,10 @@ from p2pool.util import pack
 
 
 P2P_PREFIX = 'bf0c6bbd'.decode('hex')
-P2P_PORT = 9999
+P2P_PORT = 9838
 ADDRESS_VERSION = 76
 SCRIPT_ADDRESS_VERSION = 16
-RPC_PORT = 9998
+RPC_PORT = 9338
 RPC_CHECK = defer.inlineCallbacks(lambda dashd: defer.returnValue(
             'dashaddress' in (yield dashd.rpc_help()) and
             not (yield dashd.rpc_getinfo())['testnet']
@@ -20,9 +20,9 @@ BLOCKHASH_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').g
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'DASH'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DashCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/DashCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dashcore'), 'dash.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/address/'
-TX_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/tx/'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DashCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/DashCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.uniocore'), 'unio.conf')
+#BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/block/'
+#ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/address/'
+#TX_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUST_THRESHOLD = 0.001e8
