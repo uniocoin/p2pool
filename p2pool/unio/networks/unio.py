236af331f6a022a620753c6888b2ab2d9c27be6f
Wan Hao Jun <wanhaojun118@hotmail.com>
2018-12-19 10:09:59 -0500

    modify explorer details

--- p2pool/unio/networks/unio.py
+++ p2pool/unio/networks/unio.py
@@ -21,8 +21,8 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('unio_hash').getPoWH
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'UOC'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'UnioCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/UnioCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.uniocore'), 'unio.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/address/'
-TX_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.unio.tech/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.unio.tech/address/'
+TX_EXPLORER_URL_PREFIX = 'https://explorer.unio.tech/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUST_THRESHOLD = 0.001e8
